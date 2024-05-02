af2b3c3bbc8f3d6abd86fe9a19ca97f84369435e
VanessaBustamante19 <wendy.delgado@uabc.edu.mx>
2024-05-01 21:47:29 -0700

    Agregando botón para cambiar de página
    Realicé una función la cual permite cambiar de página dentro del sitio web, una vez que se ejecuten las líneas de código anteriores

--- Web_Scrapping/funciones_web_scrapping.py
+++ Web_Scrapping/funciones_web_scrapping.py
@@ -1,1 +1,8 @@
-"""Archivo con las funciones para realizar el proceso Web Scrapping."""
+
+#Código para cambiar de página
+
+def boton_siguiente(navegador): #Se genera la función "boton_siguiente", con el parametro "navegador"
+
+    pagina = navegador.find_element(By.CSS_SELECTOR, "a.xXx[href*='/peliculas/todas-peliculas/?page=']")  
+    #La cadena de texto (CSS_SELECTOR) busca el enlace (a), que tenga como clase xXx, con atributo href el cual contiene la linea '/peliculas/todas-peliculas/?page='
+    pagina.click() #Una vez encontrada la linea anterior, da click sobre ella y ejecuta el cambio de página
