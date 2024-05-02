b5af964e448434f138127e08c1525b9531a8a804
Hortencia13 <m1293471@uabc.edu.mx>
2024-05-01 20:20:37 -0700

    creando el btn de peliculas
    Creación de el método para accionar el clic del botón películas.

--- Web_Scrapping/funciones_web_scrapping.py
+++ Web_Scrapping/funciones_web_scrapping.py
@@ -1,1 +1,6 @@
 """Archivo con las funciones para realizar el proceso Web Scrapping."""
+
+def peliculas(navegador):                                                   #Crear el método para accionar el botón.
+    btnpeliculas = navegador.find_element(By.CLASS_NAME, "header-nav-link") #Busqueda del botón películas y lo guarda en una variable llamada btnpeliculas.
+    time.sleep(5)                                                           #Espera un tiempo para realizar la acción.
+    btnpeliculas.click()                                                    #Realiza la accion de clikear sobre el boton películas.
