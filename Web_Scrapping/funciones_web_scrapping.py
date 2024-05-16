e24993a98e6f1b752b274ce8cbc23236cdb347ba
Hortencia13 <m1293471@uabc.edu.mx>
2024-05-15 23:18:53 -0700

    función para crear un dataframe
    Creación de un método que recibe como parámetro, creamos un dataframe a partir del diccionario, para que al final retorne el dataframe

--- Web_Scrapping/funciones_web_scrapping.py
+++ Web_Scrapping/funciones_web_scrapping.py
@@ -2,6 +2,7 @@
 
 """Importamos las bibliotecas necesarias en este caso selenium y webdriver-manager"""
 import time
+import pandas as pd                                     #importar la librería para crear el dataframe
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
@@ -43,3 +44,8 @@ def boton_siguiente(navegador): #Se genera la función "boton_siguiente", con el
     #La cadena de texto (CSS_SELECTOR) busca el enlace (a), que tenga como clase xXx, con atributo href el cual contiene la linea '/peliculas/todas-peliculas/?page='
     pagina.click() #Una vez encontrada la linea anterior, da click sobre ella y ejecuta el cambio de página
 
+#Función para crear un dataframe 
+
+def crear_dataframe(data:dict):             #Creación de un método que recibe como parámetro un diccionario
+    dataframe = pd.DataFrame(data)          #Creacion de un dataframe a partir de un diccionario
+    return dataframe                        #Retorna el dataframe
