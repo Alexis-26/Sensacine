d912b0aae1dc17e7760963262cd39cfb644821a4
Jacqueline-Lopez <141884681+Jacqueline-Lopez@users.noreply.github.com>
2024-05-17 08:53:28 -0700

    Creacion de la base de datos
    Realizamos la creacion de la base datos para luego empezar a insertar tablas

--- Base_de_Datos/tablas.sql
+++ Base_de_Datos/tablas.sql
@@ -32,3 +32,59 @@ Nombre VARCHAR(100));
 CREATE TABLE estreno(
 id_estreno INT PRIMARY KEY AUTO_INCREMENT,
 nombre VARCHAR(100));
+
+
+CREATE TABLE genero(
+id_genero INT PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR(100));
+
+CREATE TABLE idioma(
+id_idioma INT PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR(100));
+
+CREATE TABLE director_pelicula
+(id_directorp INT PRIMARY KEY AUTO_INCREMENT,
+id_director INT,
+id_pelicula INT,
+FOREIGN KEY (id_director) REFERENCES director (id_director),
+FOREIGN KEY (id_pelicula ) REFERENCES pelicula (id_pelicula ));
+
+CREATE TABLE genero_pelicula
+(id_generop INT PRIMARY KEY AUTO_INCREMENT,
+id_genero INT,
+id_pelicula INT,
+FOREIGN KEY (id_genero) REFERENCES genero (id_genero),
+FOREIGN KEY (id_pelicula ) REFERENCES pelicula (id_pelicula ));
+
+
+CREATE TABLE idioma_pelicula
+(id_idiomap INT PRIMARY KEY AUTO_INCREMENT,
+id_idioma INT,
+id_pelicula INT,
+FOREIGN KEY (id_idioma) REFERENCES idioma (id_idioma),
+FOREIGN KEY (id_pelicula ) REFERENCES pelicula (id_pelicula ));
+
+CREATE TABLE detalle_pelicula(
+id_detalles INT PRIMARY KEY AUTO_INCREMENT,
+fecha DATE,
+duracion VARCHAR(100),
+guion VARCHAR(100),
+id_pelicula INT,
+id_estreno INT,
+id_directorp INT,
+id_generop INT,
+id_idiomap INT,
+FOREIGN KEY (id_pelicula) REFERENCES pelicula (id_pelicula),
+FOREIGN KEY (id_estreno) REFERENCES estreno (id_estreno), 
+FOREIGN KEY (id_directorp) REFERENCES director_pelicula (id_directorp),
+FOREIGN KEY (id_generop) REFERENCES genero_pelicula (id_generop),
+FOREIGN KEY (id_idiomap) REFERENCES idioma_pelicula (id_idiomap))
+
+
+
+
+
+
+
+
+
