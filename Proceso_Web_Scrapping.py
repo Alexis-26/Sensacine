05cced6e67108a952de4eb5d36dfb45acb6c0232
Alexis-26 <alexis.miros@uabc.edu.mx>
2024-05-18 22:15:38 -0700

    Modificaciones
    Se modificó los ajustes del tamaño de la ventana cuando se ejecuta el proceso.
    Se cambio el nombre de la función a top_peliculas, y el funcionamiento de la función se modifico utilizando LINK_TEXT en lugar de XPATH.
    Se eliminaron los tiempos de espera que se encontraban en las funciones.
    Por último, para accionar el botón siguiente, se cambio la referencia que proviene del codigo html.
    Además, se realizó una parte del proceso de web scrapping en el archivo "Proceso_Web_Scrapping" utilizando las funciones desarrolladas en el documento "funciones_web_scrapping".

--- Proceso_Web_Scrapping.py
+++ Proceso_Web_Scrapping.py
@@ -1,7 +1,29 @@
 """Se crea un archivo, en el cual se desarrollará el proceso de Web Scrapping."""
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+from Web_Scrapping.funciones_web_scrapping import *
+
 def Proceso():
-    pass  
+    navegador = conexion()
+    time.sleep(5)
+    peliculas(navegador)
+    time.sleep(5)
+    top_películas(navegador)
+    time.sleep(5)
+    for i in range(2):
+        acceder_al_bloque(navegador)
+        time.sleep(10)
+        boton_regresar(navegador)
+        time.sleep(10)
+        boton_siguiente(navegador)
+        time.sleep(8)
+    
 
-
+if __name__ == "__main__":
+    Proceso()
 
 
--- Web_Scrapping/funciones_web_scrapping.py
+++ Web_Scrapping/funciones_web_scrapping.py
@@ -16,32 +16,29 @@ def conexion():
     driver = ChromeDriverManager().install()
     s = Service(driver)                                 #se crea un objeto Service utilizando la ubicacion del controlador
     opc = Options()                                     #se crea un objeto para configurar las opciones del navegador
-    opc.add_argument("--window-size=1020,1200")         #se establece le tamaño de la ventana del navegador
+    opc.add_argument("--start-maximized")         #se establece le tamaño de la ventana del navegador
     navegador = webdriver.Chrome(service=s, options=opc)#crea una instancia del navegador Chrome pasando el servicio y las opciones como argumentos
     navegador.get("https://www.sensacine.com")          #Abre la paguina web especificada
-    time.sleep(10)                                      #se espera 10 seundos
 
     return navegador #retorna la clase navegador
 
 #Función para el botón de Todas las películas
-def todas_las_películas(navegador):
+def top_películas(navegador):
     #Se genera una variable para encontrar el botón de  todas las peliculas.
-    botón = navegador.find_element(By.XPATH,"//*[@id='header-main-nav']/ul/li[1]/div/ul/li[7]/a") 
+    botón = navegador.find_element(By.LINK_TEXT,"Top películas") 
     #Aquí se ingresa al botón dándole click.
     botón.click()
 
 #Función para el botón de Películas
 def peliculas(navegador):                                                   #Crear el método para accionar el botón.
     btnpeliculas = navegador.find_element(By.CLASS_NAME, "header-nav-link") #Busqueda del botón películas y lo guarda en una variable llamada btnpeliculas.
-    time.sleep(5)                                                           #Espera un tiempo para realizar la acción.
+    
     btnpeliculas.click()                                                    #Realiza la accion de clikear sobre el boton películas.
 
 #Código para cambiar de página
 
 def boton_siguiente(navegador): #Se genera la función "boton_siguiente", con el parametro "navegador"
-
-    pagina = navegador.find_element(By.CSS_SELECTOR, "a.xXx[href*='/peliculas/todas-peliculas/?page=']")  
-    #La cadena de texto (CSS_SELECTOR) busca el enlace (a), que tenga como clase xXx, con atributo href el cual contiene la linea '/peliculas/todas-peliculas/?page='
+    pagina = navegador.find_element(By.CSS_SELECTOR, "a.xXx.button.button-md.button-primary-full.button-right")
     pagina.click() #Una vez encontrada la linea anterior, da click sobre ella y ejecuta el cambio de página
 
 #Función para crear un dataframe 
