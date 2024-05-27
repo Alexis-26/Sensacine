59fe4a1af857acc1db015cb583669bfa0bee187b
Alexis-26 <alexis.miros@uabc.edu.mx>
2024-05-27 09:10:06 -0700

    Nueva función y modificaciones
    Se creó la función para realizar el proceso de web scrapping utilizando librerías.
    En el archivo "funciones_web_scrapping" se modificaron algunas validaciones para obtención de datos.

--- Proceso_Web_Scrapping.py
+++ Proceso_Web_Scrapping.py
@@ -1,4 +1,4 @@
-"""Se crea un archivo, en el cual se desarrollará el proceso de Web Scrapping."""
+"""Importacion de librerias"""
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -6,24 +6,40 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from webdriver_manager.chrome import ChromeDriverManager
 from Web_Scrapping.funciones_web_scrapping import *
+from bs4 import BeautifulSoup
 
 def Proceso():
-    navegador = conexion()
-    time.sleep(5)
-    peliculas(navegador)
-    time.sleep(5)
-    top_películas(navegador)
-    time.sleep(5)
-    for i in range(2):
-        acceder_al_bloque(navegador)
-        time.sleep(10)
-        boton_regresar(navegador)
-        time.sleep(10)
-        boton_siguiente(navegador)
-        time.sleep(8)
-    
+    try:
+        navegador = conexion()
+        time.sleep(6)
+        peliculas(navegador)
+        time.sleep(6)
+        top_películas(navegador)
+        time.sleep(25) #Este se debe de quitar
+        for i in range(1, 30):
+            cant = len(bloques_peliculas(navegador))
+            for pelicula in range(cant):
+                lista_peliculas = bloques_peliculas(navegador)
+                time.sleep(3)
+                acceder_al_bloque(lista_peliculas[pelicula])
+                time.sleep(10)
+                btn_home(navegador)
+                time.sleep(10)
+                soup = bsoup(navegador)
+                extraccion_informacion(soup)
+                time.sleep(5)
+                boton_regresar(navegador)
+                time.sleep(5)
+                boton_regresar(navegador)
+                time.sleep(5)
+            boton_siguiente(navegador)
+            time.sleep(10)
+        df = crear_dataframe()
+        df.to_csv("Dataset/sensacine.csv")
+    except Exception as e:
+        df = crear_dataframe()
+        df.to_csv("Dataset/sensacine.csv")
+        print(e)
 
 if __name__ == "__main__":
     Proceso()
-
-
