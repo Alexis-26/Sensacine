3a75163d4e71202d5e4e456f9af1bc90e47cd862
DailySolis <141890753+DailySolis@users.noreply.github.com>
2024-06-12 15:13:58 -0700

    Se integraron funciones para crear widgets.
    Se actualizó el archivo en algunas funciones con sus respectivos comentarios.
    
    Co-Authored-By: Jacqueline-Lopez <141884681+Jacqueline-Lopez@users.noreply.github.com>

--- Dashboards/Widgets/widgets_dash.py
+++ Dashboards/Widgets/widgets_dash.py
@@ -1,15 +1,24 @@
 import dash                                                #Importamos la librería de Dash
 import dash_bootstrap_components as dbc                    #Importamos los componentes de Bootstrap para Dash
 from dash import Input, Output, dcc, html
-import pandas as pd     
-def widgets_dropdown_dir():                                                     #Se realizó una función para la visualización de un botón desplegable
+import pandas as pd  
+
+"""
+Se realizó una función para la visualización de un botón desplegable,S+se despliega usuarios, 
+medios y sensacine, predeterminando viaualizualmente Usuarios
+se le agrega un id para llamarlo en el callback
+"""
+def widgets_dropdown_dir():                                                     
     califdir = html.Div([                   
-    dcc.Dropdown(["Usuarios", "Medios", "Sensacine"], "Usuarios", id="ddcalf",) #Se despliega usuarios, medios y sensacine, predeterminando viaualizualmente Usuarios
-    ])                                                                         #Se le agrega un id para llamarlo en el callback
+    dcc.Dropdown(["Usuarios", "Medios", "Sensacine"], "Usuarios", id="ddcalf",)
     return califdir
 
-def widget_rslider(data:pd.DataFrame):                                          #Se realizó una función para poder visualizar un botón de rango
-    max_val= len(data)                                                          #Sirve para agregar la cantidad de datos que se quieren ver en los dashboards.
+"""
+Se realizó una función para poder visualizar un botón de rango,
+sirve para agregar la cantidad de datos que se quieren ver en los dashboards.
+"""
+def widget_rslider(data:pd.DataFrame):                                         
+    max_val= len(data)                                                          
     rango = dcc.RangeSlider(1, max_val,10, value=[0, 10], id="tcalf",
                             tooltip={"placement": "bottom", "always_visible": True})
     return rango
@@ -24,6 +33,7 @@ def widget_radio():
     Comparacion_prs = dcc.RadioItems(["Presupuesto total",
         "Presupuesto promedio"], id="radmpresupuesto", value="Presupuesto total", inline=True)
     return Comparacion_prs
+
 """
 Se creo una función dónde el widget  es de opción, este  selecciona uno 
 de los botones  que esten en este seria presupuesto total y presupuesto promedio
@@ -36,7 +46,10 @@ def widget_cambios():
                      "Total de presupuesto", id="ddapresupuesto")
     ])
     return cambio_presupuesto
-
+"""
+Se realizó una función para poder visualizar un botón de rango,
+sirve para agregar el rango en años, que se se reflejará en los dashboards.
+"""
 def widget_rslider_tiempo(data:pd.DataFrame):
     min_val = data["año"].min()
     max_val = data["año"].max()
